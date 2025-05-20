81160e63e8d602f57932fd3bbb25a66b861d8ed9
dsutomo <dsutomo@deloitte.com>
2025-05-20 19:21:46 +0700

    data service : updated parent pom to include jar, redis config created

---
+++ data-service/src/main/java/com/techcombank/tclife/dataService/service/RedisService.java
@@ -0,0 +1,28 @@
+package com.techcombank.tclife.dataService.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void saveData(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public void saveOrUpdateObject(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+}
