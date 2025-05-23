3558451da15ac239e831529c23c3d6c033a083af
dsutomo <dsutomo@deloitte.com>
2025-05-23 14:31:34 +0700

    data service : multiple language save master table

--- data-service/src/main/java/com/techcombank/tclife/dataService/controller/DataController.java
+++ data-service/src/main/java/com/techcombank/tclife/dataService/controller/DataController.java
@@ -8,7 +8,9 @@ import com.techcombank.tclife.dataService.service.DataService;
 import com.techcombank.tclife.dataService.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -17,7 +19,8 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Validated
-public class DataController implements  DataAPI{
+@RequestMapping("/test")
+public class DataController implements DataAPI{
 
     private final DataService dataService;
     private final RedisService redisService;
@@ -32,7 +35,58 @@ public class DataController implements  DataAPI{
     }
 
     @Override
+    public List<MasterTable> saveVNMasterStatus() {
+        return  dataService.saveMasterTable("vn_VN", "TProposalStatus");
+    }
+
+    @Override
+    public List<MasterTable> saveVNMarriage() {
+        return  dataService.saveMasterTable("vn_VN", "TMarriage");
+    }
+
+    @Override
+    public List<MasterTable> saveENMarriage() {
+        return  dataService.saveMasterTable("en_US", "TMarriage");
+    }
+
+    @Override
+    public List<MasterTable> saveVNOccupation() {
+        return  dataService.saveMasterTable("vn_VN", "TOccupation");
+    }
+
+    @Override
+    public List<MasterTable> saveENOccupation() {
+        return  dataService.saveMasterTable("en_US", "TOccupation");
+    }
+
+    @Override
+    public List<MasterTable> saveVNGender() {
+        return  dataService.saveMasterTable("vn_VN", "TGender");
+    }
+
+    @Override
+    public List<MasterTable> saveENGender() {
+        return  dataService.saveMasterTable("en_US", "TGender");
+    }
+
+    @Override
+    public List<MasterTable> saveVNGRelationshipInsured() {
+        return  dataService.saveMasterTable("vn_VN", "TRelationshipToMainInsured");
+    }
+
+    @Override
+    public List<MasterTable> saveENRelationshipInsured() {
+        return  dataService.saveMasterTable("en_US", "TRelationshipToMainInsured");
+    }
+
+    @Override
     public List<MasterTable> getENStatusPayload() {
         return (List<MasterTable>) redisService.getData("payloadStatusENG");
+    }
+
+    @Override
+    @GetMapping("/en_US/retrieveOccupation")
+    public List<MasterTable> getENOccupationList() {
+        return (List<MasterTable>) redisService.getData("occupationENG");
     }
 }
