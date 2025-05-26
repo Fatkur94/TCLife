ecf5458bb530393c8c1326fa6b98c31223585c20
dsutomo <dsutomo@deloitte.com>
2025-05-26 15:26:48 +0700

    policy service :fix feign configuration

--- policy-service/src/main/java/com/techcombank/tclife/policyService/client/ProposalPolicyIntegrationClient.java
+++ policy-service/src/main/java/com/techcombank/tclife/policyService/client/ProposalPolicyIntegrationClient.java
@@ -4,7 +4,7 @@ import com.techcombank.tclife.common.model.dto.policy.ProposalPolicyPayload;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "integration-service", url = "${integration.portal.data-service.base-url}")
+@FeignClient(name = "integration-service", url = "${integration.portal.integration-service.base-url}")
 public interface ProposalPolicyIntegrationClient {
     @GetMapping("/retrievePayloadToTransform")
     ProposalPolicyPayload getPayloadToTransform();
